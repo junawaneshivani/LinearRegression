b3f1fc5670dfb294de981ff6a3e55017666dcf22
Shivani Junawane <shivanij@us.ibm.com>
2019-12-11 14:11:39 +0530

    Slpitted input data to calculate accuracy

--- LinearRegression.py
+++ LinearRegression.py
@@ -88,11 +88,9 @@ def plot_graph_and_line(X, y, y_pred) -> None:
     Output: points and lines on graph
     """
     plt.title("Linear Regression")
-    plt.scatter(X, y)
+    plt.scatter(X, y, color="green")
     plt.plot(X, y_pred, color="red")
     plt.show(block=False)
-
-
 
 if __name__ == "__main__":
     
@@ -100,31 +98,37 @@ if __name__ == "__main__":
     X    = np.array( data['Hours spent Studying'])                      # values converts it into a numpy array
     y    = np.array( data['Marks scored'])                              # -1 means that calculate the dimension of rows, but have 1 column
 
+    X_train, X_test = X[:80], X[80:]                                    # split data to calculate accuracy later
+    y_train, y_test = y[:80], y[80:]
+
     theta0        = 10
     theta1        = 0.1
-    learning_rate = 0.0002
-
-    y_pred = get_predicted_y(theta0, theta1, X)                         # calculate y_pred for current values of theta
-    mse = mean_squared_error(y, y_pred)                                 # calculate mean square error
-    plot_graph_and_line(X, y, y_pred)
+    learning_rate = 0.0001
+    
+    y_pred = get_predicted_y(theta0, theta1, X_train)                   # calculate y_pred for current values of theta
+    mse = mean_squared_error(y_train, y_pred)                           # calculate mean square error
+    plot_graph_and_line(X_train, y_train, y_pred)
     
     for i in range(1000):
-        theta0, theta1 = gradient_descent(theta0, theta1, learning_rate, X, y, y_pred)  # new values of theta
-        y_pred = get_predicted_y(theta0, theta1, X)                                     # calculate y_pred for current values of theta
-        new_mse = mean_squared_error(y, y_pred)                                         # calculate mean square error
+        theta0, theta1 = gradient_descent(theta0, theta1, learning_rate, X_train, y_train, y_pred)  # new values of theta
+        y_pred = get_predicted_y(theta0, theta1, X_train)                                           # calculate y_pred for current values of theta
+        new_mse = mean_squared_error(y_train, y_pred)                                               # calculate mean square error
         
         print (" Theta0: {}, Theta1: {}, iteration: {}, MSE: {} ".format(theta0, theta1, i, new_mse))
         
         if math.isclose(mse, new_mse, abs_tol=1e-05):                   # stopping the gradient descent if the mse does not change by more than 1e-05
             print(" Breaking.. Found ideal value of theta0 and theta1.. ")
-            plot_graph_and_line(X, y, y_pred)
+            plot_graph_and_line(X_train, y_train, y_pred)
             break       
         
         if (new_mse < mse):
             mse = new_mse    
-        if i % 50 == 0:
-            plot_graph_and_line(X, y, y_pred)
+        if i % 20 == 0:
+            plot_graph_and_line(X_train, y_train, y_pred)
     
-        
+    y_pred = get_predicted_y(theta0, theta1, X_test)                    # Calculating accuracy on test data
+    accuracy = "Accuracy: " + str(mean_squared_error(y_test, y_pred))
+    
+    plt.text(30, 120, accuracy, bbox=dict(facecolor='red', alpha=0.5))    
     plt.show()
     
